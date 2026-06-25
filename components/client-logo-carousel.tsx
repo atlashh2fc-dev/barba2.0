7d12b75a83a11fce18896df806893ff243101522
Atlas Dev <atlashormazabal@gmail.com>
2026-06-25 15:26:57 -0400

    Remove client logo carousel background

--- components/client-logo-carousel.tsx
+++ components/client-logo-carousel.tsx
@@ -3,13 +3,13 @@ import { clientLogos } from "@/lib/content";
 
 export function ClientLogoCarousel() {
   return (
-    <div className="client-logo-carousel overflow-hidden border border-[#071a38]/10 bg-white">
+    <div className="client-logo-carousel overflow-hidden">
       <div className="client-logo-track flex w-max">
         {[...clientLogos, ...clientLogos].map((client, index) => (
           <div
             key={`${client.name}-${index}`}
             aria-hidden={index >= clientLogos.length}
-            className="flex h-28 w-[220px] shrink-0 items-center justify-center border-r border-[#071a38]/8 px-7 sm:h-32 sm:w-[260px]"
+            className="flex h-28 w-[220px] shrink-0 items-center justify-center px-7 sm:h-32 sm:w-[260px]"
           >
             <Image
               src={client.image}
